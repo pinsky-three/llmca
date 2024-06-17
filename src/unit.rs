0d03f412d6ea0bb6ad448182c97653f330b8f872
Bregy Malpartida <bregy@minsky.cc>
2024-06-17 14:15:14 -0500

    refactor: Update CognitiveUnit struct to include input and output structs

--- src/unit.rs
+++ src/unit.rs
@@ -1,6 +1,8 @@
 use std::{env, time::Duration};
 
 use reqwest::header;
+
+use serde_derive::{Deserialize, Serialize};
 use serde_json::json;
 
 use crate::api::ChatCompletionResponse;
@@ -12,28 +14,90 @@ pub struct CognitiveUnit {
     pub position: (usize, usize),
 }
 
+#[derive(Debug, Clone, Serialize)]
+struct CognitiveUnitInput {
+    rule: String,
+    state: Vec<String>,
+    neighbors: Vec<(String, Vec<String>)>,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+struct CognitiveUnitOutput {
+    next_state: Vec<String>,
+}
+
 impl CognitiveUnit {
-    pub fn calculate_next_state(&self, neighbors: Vec<(String, Vec<String>)>) -> String {
+    pub fn calculate_next_state(&self, neighbors: Vec<(String, Vec<String>)>) -> Vec<String> {
         self.llm_model_call(neighbors)
     }
 
-    pub fn llm_model_call(&self, neighbors: Vec<(String, Vec<String>)>) -> String {
-        let system_message = format!("{{ \"rule\": {:?} }}", self.rule);
+    pub fn llm_model_call(&self, neighbors: Vec<(String, Vec<String>)>) -> Vec<String> {
+        let system_message = "
+        You're a LLM Cognitive Unit and your unique task is to respond with your next 
+        state based on the state of your neighbors in json format based on:
+        
+        #[derive(Debug, Clone, Serialize)]
+        struct CognitiveUnitInput {
+            rule: String,
+            state: Vec<String>, // json sequence of states as any kind of string
+            neighbors: Vec<(String, Vec<String>)>, // json sequence of (name, state)
+        } 
 
-        let input_message = format!(
-            "{{ \"neighbors\": {:?}, \"state\": {:?} }}",
-            neighbors, self.state
-        );
+        #[derive(Debug, Clone, Deserialize)]
+        struct CognitiveUnitOutput {
+            next_state: Vec<String>, // json sequence of states as any kind of string
+        }
 
-        let res = Self::generic_chat_completion(system_message, input_message);
+        example input:
+        {
+            \"rule\": \"You're a cellular automaton with game of life behavior. 
+            Response with your next state based on the state of your neighbors.
+            Don't response with explanations or nothing else, only your state.
+            Always respond with a single state and ignoring the rest.\",
+            \"state\": [\"0\"],
+            \"neighbors\": [
+                [\"n_0\", [\"1\"]],
+                [\"n_1\", [\"0\"]],
+                [\"n_2\", [\"1\"]],
+                [\"n_3\", [\"0\"]],
+                [\"n_4\", [\"1\"]],
+                [\"n_5\", [\"0\"]],
+                [\"n_6\", [\"0\"]],
+                [\"n_7\", [\"0\"]]
+            ]
+        }
 
-        res.unwrap()
-            .choices
-            .first()
-            .unwrap()
-            .clone()
-            .message
-            .content
+        example output:
+        {
+            \"next_state\": [\"1\"]
+        }
+        "
+        .to_string();
+
+        let input_payload = serde_json::to_string_pretty(&CognitiveUnitInput {
+            rule: self.rule.clone(),
+            state: self.state.clone(),
+            neighbors,
+        })
+        .unwrap();
+
+        let res = Self::generic_chat_completion(system_message, input_payload);
+
+        serde_json::from_str::<CognitiveUnitOutput>(
+            &res.unwrap()
+                .choices
+                .first()
+                .unwrap()
+                .clone()
+                .message
+                .content,
+        )
+        .map(|o| o.next_state)
+        .unwrap_or_else(|err| {
+            println!("Error in LLM model call ({:?}): {:?}", self.position, err);
+
+            self.state.clone()
+        })
     }
 
     fn generic_chat_completion(
