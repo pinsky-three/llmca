cd929694daae44effee5c173585748598395a38a
Bregy Malpartida <bregy@minsky.cc>
2024-06-17 12:20:21 -0500

    refactor: Remove commented out code and unused functions in system.rs

--- src/system.rs
+++ src/system.rs
@@ -144,36 +144,6 @@ where
             }
         });
 
-        // for i in 0..n {
-        //     for j in 0..m {
-        //
-        //
-
-        //         let i_s1 = i.overflowing_sub(1).1.then(|| n - 1).unwrap_or(i);
-        //         let j_s1 = j.overflowing_sub(1).1.then(|| m - 1).unwrap_or(j);
-
-        //         let i_a1 = (i + 1) % n;
-        //         let j_a1 = (j + 1) % m;
-
-        //         space.add_connection(xy_to_index(i, j), xy_to_index(i_a1, j)); // n
-        //                                                                        // space.add_connection(xy_to_index(i, j), xy_to_index(i_a1, j_a1)); // ne
-        //         space.add_connection(xy_to_index(i, j), xy_to_index(i, j_a1)); // e
-        //                                                                        // space.add_connection(xy_to_index(i, j), xy_to_index(i_s1, j_a1)); // se
-        //         space.add_connection(xy_to_index(i, j), xy_to_index(i_s1, j)); // s
-        //                                                                        // space.add_connection(xy_to_index(i, j), xy_to_index(i_s1, j_s1)); // sw
-        //         space.add_connection(xy_to_index(i, j), xy_to_index(i, j_s1)); // w
-        //                                                                        // space.add_connection(xy_to_index(i, j), xy_to_index(i_a1, j_s1));
-        //                                                                        // nw
-        //     }
-        // }
-
-        // println!("space.connections: {:?}", space.connections.len());
-        // println!("space.units: {:?}", sp,ace.units.len());
-
-        // graph.edges(nodes[0]).for_each(|edge| {
-        //     println!("edge: {:?}", edge);
-        // });
-
         CognitiveSpace {
             _rule: Box::new(rule),
             graph,
@@ -185,22 +155,6 @@ impl<R> CognitiveSpace<R>
 where
     R: CognitiveRule + Debug + Clone,
 {
-    // pub fn step_sync(&self) {
-    //     self.connections.iter().for_each(|(from, to)| {
-    //         let from_unit = &self.units[*from];
-    //         let to_unit = &self.units[*to];
-
-    //         let neighbors = vec![(from_unit.rule.clone(), from_unit.state.clone())];
-
-    //         let _next_state = to_unit.calculate_next_state(neighbors);
-    //     });
-
-    //     println!(
-    //         "Running cognitive space with rule: {:?}",
-    //         self.rule.get_rule_prompt()
-    //     );
-    // }
-
     pub fn sync_step(&mut self) {
         self.graph.clone().node_indices().for_each(|node| {
             let neighbors = self
@@ -230,21 +184,6 @@ where
     }
 
     pub fn generate_graph(&self) -> StableGraph<CognitiveUnit, (), Undirected> {
-        // let mut nodes = vec![];
-        // let mut g = StableUnGraph::with_capacity(self.units.len(), self.connections.len());
-
-        // self.units.iter().for_each(|_unit| {
-        //     let node = g.add_node(());
-        //     nodes.push(node);
-        // });
-
-        // self.connections.iter().for_each(|(from, to)| {
-        //     let from = nodes[*from];
-        //     let to = nodes[*to];
-
-        //     g.add_edge(from, to, ());
-        // });
-
         self.graph.clone()
     }
 
